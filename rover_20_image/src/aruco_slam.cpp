316e170dc9da704c26894d9050a639b74c77c1aa
Berke Algül <47500887+berkealgul@users.noreply.github.com>
2020-07-19 21:22:22 +0300

    Güncelleme
    -Algoritma iyileştirmesi yapıldı 
    -Çoklu artagler ile kamera güncellenmesi yapılabilir (test edilmedi)
    -Kod Temizliği

--- rover_20_image/src/aruco_slam.cpp
+++ rover_20_image/src/aruco_slam.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <tuple>
 #include <opencv2/opencv.hpp>
 #include <opencv2/aruco.hpp>
 #include <ros/ros.h>
@@ -17,9 +18,11 @@ using namespace cv;
 using namespace aruco;
 
 void broadcast_tf(tf::TransformBroadcaster br, slam_obj *obj);
-slam_obj *create_camera();
-void update_cam_transform(tf::Quaternion R_cm, tf::Vector3 T_cm ,slam_obj *camera, slam_obj *marker);
+std::tuple<tf::Quaternion, tf::Vector3> estimate_cam_transform(tf::Quaternion R_cm, tf::Vector3 T_cm ,slam_obj *camera, slam_obj *marker);
 slam_obj *create_marker_obj(tf::Quaternion R_cm, tf::Vector3 T_cm, int id, slam_obj* camera);
+slam_obj *create_camera();
+tf::Quaternion avarage_quaternions(std::vector<tf::Quaternion> quaternions);
+tf::Vector3 avarage_vectors(std::vector<tf::Vector3> vectors)
 
 
 int main(int argc, char **argv)
@@ -71,13 +74,16 @@ int main(int argc, char **argv)
 		detectMarkers(frame, dictionary, corners, ids ,params);
 		estimatePoseSingleMarkers(corners, 0.2, mtx, dist, rvecs, tvecs);
 
+		std::vector<tf::Quaternion> cam_orientations;
+		std::vector<tf::Vector3>    cam_locations;
+
 		if(ids.size() > 0)
 		{
 			slam_obj *cam = tree.search_id(-1);
 
 			drawDetectedMarkers(frame, corners);
 
-			for (int i = 0, n = ids.size(); i < n; ++i)
+			for (int i = 0, n = ids.size(); i < n; i++)
 			{
 				int id = ids[i];
 				Vec3d r = rvecs[i];
@@ -92,6 +98,8 @@ int main(int argc, char **argv)
 				slam_obj *marker = tree.search_id(id);
 
 				// If marker is not exist in map(slam system) we will add it
+				// Else estimate cam transform with it 
+				// Note: Markers are assumed to be static
 				if(marker == NULL)
 				{
 					marker = create_marker_obj(R_cm, T_cm, id, cam);
@@ -99,12 +107,21 @@ int main(int argc, char **argv)
 				}
 				else
 				{
-					update_cam_transform(R_cm, T_cm, cam, marker);
+					tf::Quaternion R;
+					tf::Vector3 T;
+
+					std::tie(R, T) = estimate_cam_transform(R_cm, T_cm, cam, marker);
+
+					cam_orientations.push_back(R);
+					cam_locations.push_back(T);
 				}
-				broadcast_tf(br, marker);
-				broadcast_tf(br, cam);
 			}
+
+			cam->R = avarage_quaternions(cam_orientations);
+			cam->T = avarage_vectors(cam_locations);
 		}
+
+		//TODO: Ağacı dolaşarak her objenin tf'sini yayınlamalısın
 
 		imshow("frame", frame);
 
@@ -128,34 +145,37 @@ slam_obj *create_marker_obj(tf::Quaternion R_cm, tf::Vector3 T_cm, int id, slam_
 
 	marker->id = id;
 	marker->name = "id="+std::to_string(id);
-	marker->r = R_wm;
-	marker->t = T_wm;
+	marker->R = R_wm;
+	marker->T = T_wm;
 	marker->left = NULL;
 	marker->right = NULL;
 
 	return marker;
 }
 
-void update_cam_transform(tf::Quaternion R_cm, tf::Vector3 T_cm ,slam_obj *camera, slam_obj *marker)
+std::tuple<tf::Quaternion, tf::Vector3>  estimate_cam_transform(tf::Quaternion R_cm, tf::Vector3 T_cm ,slam_obj *camera, slam_obj *marker)
 {
-	tf::Vector3 T_wm = marker->t, T_wc = camera->t;
-	tf::Quaternion R_wm = marker->r, R_wc = camera->r;
+	tf::Vector3 T_wm = marker->T, T_wc = camera->T;
+	tf::Quaternion R_wm = marker->R, R_wc = camera->R;
 
 	T_wc = T_wm - (tf::Matrix3x3(R_wc) * T_cm);
 	R_wc = R_wm * R_cm.inverse();
 
-	camera->t = T_wc;
-	camera->r = R_wc;
+	return std::make_tuple(R_wc, T_wc);
 }
 
 slam_obj *create_camera()
 {
 	slam_obj *camera = new slam_obj;
-
+	//we assumed that all markers have positive id.
+	//hence with -1 id, camera can be in tree while 
+	//can be distinguished from markers
 	camera->id = -1;
 	camera->name = "c922";
-	camera->r.setRPY(PI/2, PI, 0);
-	camera->t = tf::Vector3(0, 0, 0);
+	//the initial rotation and position of the camera can be changed 
+	//but not recommended
+	camera->R.setRPY(PI/2, PI, 0);
+	camera->T = tf::Vector3(0, 0, 0);
 	camera->left = NULL;
 	camera->right = NULL;
 	
@@ -168,4 +188,31 @@ void broadcast_tf(tf::TransformBroadcaster br, slam_obj *obj)
 	transform.setOrigin(obj->t);
 	transform.setRotation(obj->r);
 	br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), FIXED_FRAME, obj->name));
+}
+
+tf::Quaternion avarage_quaternions(std::vector<tf::Quaternion> quaternions)
+{
+	tf::Quaternion I, avg;
+	I.setRPY(0, 0, 0);
+	avg.setRPY(0, 0, 0);
+
+	int n = quaternions.size();
+	float weight = 1.0f / (float)n;
+
+	for(int i = 0,; i < n; i++)
+		avg *= I.slerp(quaternions[i], weight);
+
+	return avg;
+}
+
+tf::Vector3 avarage_vectors(std::vector<tf::Vector3> vectors)
+{
+	tf::Vector3 avg = tf::Vector3(0, 0, 0);
+	int n = vectors.size();
+
+	for(int i = 0; i < n; i++)
+		avg += vectors[i];
+	avg /= (float)n;
+
+	return avg;
 }
